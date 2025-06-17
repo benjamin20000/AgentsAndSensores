9faf2152f8e6e7227ee73d22d4453eb3e94836e9
benjamin <2000beni770@gmail.com>
2025-06-17 12:29:38 +0300

    creat abstract agent class

--- agentsANDsensors/agents/Agent.cs
+++ agentsANDsensors/agents/Agent.cs
@@ -1,6 +1,45 @@
-namespace agentsANDsensors.agents;
+namespace agentsANDsensors;
 
-public class Agent
+public abstract class Agent
 {
+    public GameEnums.AgentEnum AgentType;
+    public int weaknessesNum { get;}
+    public int discoveredWeaknesses{get;set;}
+    public GameEnums.SensorEnum[] agentWeaknesses;
+    public Sensor[] agentSensors;
+
+    public Agent(GameEnums.AgentEnum agentType)
+    {
+        discoveredWeaknesses = 0;
+        weaknessesNum = (int)(agentType);
+        agentSensors = new Sensor[weaknessesNum];
+        agentWeaknesses = new GameEnums.SensorEnum[weaknessesNum]; //create the weakness arr in the size of the agent rank
+        createAgentWeakness();//fill it with randomly sensors
+    }
+
+    public void createAgentWeakness()
+    {
+        var allSensors = GameEnums.getSensors();
+        Random rnd = new Random();
+        for(int i = 0; i < agentWeaknesses.Length; i++)
+        {
+            int sns_rnd  = rnd.Next(0, allSensors.Length);
+            agentWeaknesses[i] = allSensors[sns_rnd];
+        }
+    }
+
+    public void printAgentWeakness()
+    {
+        foreach (var sensor in agentWeaknesses)
+        {
+            Console.WriteLine(sensor);
+        }
+    }
+
+    public GameEnums.SensorEnum[] getUnrevealedWeaknesses()
+    {
+        return (GameEnums.SensorEnum[])(agentWeaknesses.Where(weakness => weakness != GameEnums.SensorEnum.NullSensor).ToArray());
+    }
+
     
 }
