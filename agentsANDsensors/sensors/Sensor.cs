b430df5e8172745fa4572870205c97fb48e7a617
benjamin <2000beni770@gmail.com>
2025-06-17 12:30:45 +0300

    abstract sensor class

--- agentsANDsensors/sensors/Sensor.cs
+++ agentsANDsensors/sensors/Sensor.cs
@@ -1,6 +1,25 @@
-namespace agentsANDsensors.sensors;
+namespace agentsANDsensors;
 
-public class Sensor
+public abstract class Sensor
 {
-    
+    private GameEnums.SensorEnum type;
+
+    public Sensor(GameEnums.SensorEnum type)
+    {
+        this.type = type;
+    }
+    public virtual void Activate(Agent agent)
+    {
+        for (int i = 0; i < agent.agentWeaknesses.Length; i++)
+        {
+            if (agent.agentWeaknesses[i] == this.type)
+            {
+                agent.agentWeaknesses[i] = GameEnums.SensorEnum.NullSensor;
+                agent.agentSensors[agent.weaknessesNum - agent.discoveredWeaknesses-1] = this;
+                agent.discoveredWeaknesses++;
+                break;
+            }
+        }
+    }
+
 }
