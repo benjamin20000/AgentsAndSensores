ea93fbfba90f96d5bf1ec6eb295d7d63b58fce89
benjamin <2000beni770@gmail.com>
2025-06-18 11:13:38 +0300

    add the new agent

---
+++ agentsANDsensors/agents/SeniorCommander.cs
@@ -0,0 +1,20 @@
+namespace agentsANDsensors;
+
+public class SeniorCommander: Agent
+{
+    public SeniorCommander(): base(GameEnums.AgentEnum.SeniorCommander){}
+
+    protected override void counterAttack()
+    {
+        if (base.turnsCounter % 3 == 0)
+        {
+            RemoveRandomSensors(2);
+        }
+    }
+
+    public override void ActivateSensors()
+    {
+        base.ActivateSensors();
+        counterAttack();
+    }
+}
